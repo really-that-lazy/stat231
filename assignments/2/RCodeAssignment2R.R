c60d15d208ed40cfb7175e10131ef57fc437f4e7
bruce <johnnybgoode@rogers.com>
2016-10-27 21:04:34 -0400

    ...

--- assignments/2/RCodeAssignment2R.R
+++ assignments/2/RCodeAssignment2R.R
@@ -4,12 +4,13 @@ library(moments)
 library(MASS)     # truehist is in the library MASS
 #
 ###################################################################################
-
+cat("\n\nProblem 1\n")
 ###################################################################################
 # Problem 1: R code for Binomial relative likelihood function
 # The following R code will plot the Binomial relative likelihood function and the line y = 0.15
 # which can be used to determine a 15% likelihood interval.
-id<-20456484
+##id<-20456484
+id<-20466075
 set.seed(id)
 #generate a random value of theta from Beta distribution last 2 digits of ID avoiding zero values
 theta<-rbeta(1, max(1,id-10*trunc(id/10)), max(1,trunc(id/10)-10*trunc(id/100)))  
@@ -23,18 +24,22 @@ s<-(thetahat*(1-thetahat)/n)^0.5
 th<-seq(max(0,thetahat-4*s), min(1,thetahat+4*s),0.001)
 # create function to calculate Binomial relative likelihood function
 BinRLF <- function(x) {exp(y*log(x/thetahat)+(n-y)*log((1-x)/(1-thetahat)))}
+
+png("problem1-1.png", width=680, height=480, res=120)
 plot(th,BinRLF(th),xlab="theta",type="l",lwd=2)    # plot relative likelihood function
 # draw a horizontal line at 0.15
 abline(a=0.15,b=0,col="red",lwd=2)
 title(main="Binomial Relative Likelihood Function")
+dev.off()
 ###################################################################################
 
 ###################################################################################
 # R code for finding the endpoints of a 15% likelihood interval for Binomial example
-uniroot(function(x) BinRLF(x)-0.15,lower=0.1,upper=0.15)
-uniroot(function(x) BinRLF(x)-0.15,lower=0.35,upper=0.42)
+cat("Root Finder")
+uniroot(function(x) BinRLF(x)-0.15,lower=0.1,upper=0.20)
+uniroot(function(x) BinRLF(x)-0.15,lower=0.35,upper=0.62)
 ###################################################################################
-
+cat("\n\nProblem 2\n")
 ###################################################################################
 # Problem 2: R code for Exponential relative likelihood function
 # The following R code will plot the Exponential relative likelihood function and the line y = 0.15
@@ -52,35 +57,47 @@ s<-thetahat/(n^0.5)
 th<-seq(max(0,thetahat-4*s), thetahat+4*s,0.001)
 # create function to calculate Exponential relative likelihood function 
 ExpRLF<-function(x) {(thetahat/x)^n*exp(n*(1-thetahat/x))}
+
+png("problem2-1.png", width=680, height=480, res=120)
 plot(th,ExpRLF(th),xlab="theta",type="l",lwd=2)    # plot relative likelihood function
 # draw a horizontal line at 0.15
 abline(a=0.15,b=0,col="red",lwd=2)
 title(main="Exponential Relative Likelihood Function")
+dev.off()
 ###################################################################################
 
 ###################################################################################
 # R code for finding the endpoints of a 15% likelihood interval for Exponential example
-uniroot(function(x) ExpRLF(x)-0.15,lower=2.8,upper=3)
-uniroot(function(x) ExpRLF(x)-0.15,lower=5,upper=5.5)
+uniroot(function(x) ExpRLF(x)-0.15,lower=2.8,upper=4)
+uniroot(function(x) ExpRLF(x)-0.15,lower=5,upper=8)
 ###################################################################################
-
+cat("\n\nProblem 3\n")
 #############################################################################
 # Problem 3: Sampling Distribution
 set.seed(id)
 # pop is a vector of 500 variate values for a finite population
 pop<-rpois(500,max(1,id-10*trunc(id/10)))
 mu<-mean(pop)             # population mean
+stdeviation<-sd(pop)
+cat('mu')
 mu
+cat('stdev')
+stdeviation
 (499*var(pop)/500)^0.5    # population standard deviation
+png("problem3-1.png", width=680, height=480, res=120)
 truehist(pop,h=1,main="Population Histogram",xlab="Variate Value",)
+dev.off()
 k<-10000           # number of simulations
 n<-15              # sample size
 sim<-rep(0,k)      # vector to store sample means
 # Calculate k sample means for samples of size n drawn from population pop
 for (i in 1:k)
 sim[i]=mean(sample(pop,n,replace=F)) 
+
+png("problem3-2.png", width=680, height=480, res=120)
 truehist(sim,h=0.25,xlab="Sample Mean",main="Sampling Distribution of Sample Mean")
 # percentage of times sample mean is within 0.5 of true mean mu
 mean(abs(sim-mu)<0.5)
+dev.off()
 #############################################################################
 
